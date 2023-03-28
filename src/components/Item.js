caf8acb2d5094f5a0850aceacb315b1472771d5d
Mark Nosal <mark.c.nosal@gmail.com>
2023-03-28 18:28:08 -0500

    completed first 2 deliverables

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, cartHandler] = useState(false)
+  const liClass = inCart ? 'in-cart' : ''
+
+  function handleClick() {
+    cartHandler((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? 'remove' : 'add'} onClick={handleClick}>{inCart ? 'Remove From' : 'Add to'} Cart</button>
     </li>
   );
 }
